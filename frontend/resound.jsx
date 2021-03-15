838574dd25e86cc9d7778ff08017cdef556dc982
Ray <raysidarta@gmail.com>
2021-03-15 19:47:45 -0400

    work on bugs

--- frontend/resound.jsx
+++ frontend/resound.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore  from './store/store';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import configureStore  from './store/store';
 
 
 // document.addEventListener('DOMContentLoaded', ()=>{
