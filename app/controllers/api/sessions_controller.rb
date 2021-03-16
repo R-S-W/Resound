8b041ecd9c1722f403bb6f2e0c9317292b51618c
Ray <raysidarta@gmail.com>
2021-03-16 09:29:55 -0400

    finish sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
-class SessionsController < ApplicationController
+class Api::SessionsController < ApplicationController
+
+  def create 
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password]);
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ['Invalid username or password'], status: 401
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      render 'api/users/show'
+    else
+      render json: ['There is no one logged in.'], status: 404
+    end
+  end
+
+
+
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,23 +1,25 @@
-class UsersController < ApplicationController
+class Api::UsersController < ApplicationController
 
   def create
     @u = User.new(user_params)
     if @u.find_by(username: user_params[:username])
       render json: ['Username already taken.'],status: 401     ###
     elsif @u.save!
+      login!(@u)
       render :show
     else
-      render json: ['Invalid username or password.  Please try again.']
+      render json:  @user.errors.full_messages, status: 422 #['Invalid username or password.  Please try again.']
     end
   end
   
   def destroy
-    @u = User.find_by_credentials(user_params[:username], user_params[:password])
+    @u = current_user ##User.find_by_credentials(user_params[:username], user_params[:password])
     if @u
+      logout
       @u.destroy
-      true      ####render somewhere
+      render 'api/users/show'    ####render somewhere
     else
-      render json: ['Invalid username or password.  Please try again.']
+      render json: ['Invalid username or password.  Please try again.'], status: 404
   end
 
 
