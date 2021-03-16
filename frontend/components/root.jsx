ca78cbad3af2c34c2db6267ec06772387a67d865
Ray <raysidarta@gmail.com>
2021-03-16 11:13:38 -0400

    changed name of app.jsx to App.jsx

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import {Provider} from 'react-redux';
-import App from './app';
+import App from './App';
 import {HashRouter} from 'react-router-dom';
 
 const Root = ({store})=>{
